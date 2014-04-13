42639f0744c450fc89249fe87cdb978fd820bf01
abhishekgoyal1994 <abhishekgoyalbti@gmail.com>
2014-04-13 22:48:00 +0530

    display content

--- populate.php
+++ populate.php
@@ -1,5 +1,5 @@
 <?php
-        
+    echo '<link rel="stylesheet" href="css/dropdown.css" type="text/css">';
     session_start();
     $user = $_SESSION['user'];
     $query = "SELECT * FROM filesystem WHERE owner='$user'";
@@ -14,11 +14,30 @@
     {
         echo "Unable to access database.<br>";
     }
+   
+    echo '<div class="tables">';
+    echo '<table border="1px" width="100%">';
+    echo '<tr>
+        <td>File Name</td>
+        <td>Type</td>
+        <td>Date Modified</td>
+    </tr>';
     while($row = mysqli_fetch_array($result))
     {
         $temp = explode(".", $row['file_name']);
         $ext = end($temp);
+        echo '<tr><td width="50%">'; 
         echo "<a href='files/".$row['file_id'].".".$ext."'>".$row['file_name']."</a><br>";
+        echo "</td>";
+        echo'<td width="20%">';
+        echo "image";
+        echo"</td >";
+        echo '<td width="30%">';
+        echo "04-04-2014";
+        echo"</td>";
+        echo "</tr>";
     }
+    echo "</table>";
+    echo '</div>';
 
 ?>
