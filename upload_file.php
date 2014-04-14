e27e288bd8469cc02c0c2743b0d916053c640769
ranu010101 <ranu010101@outlook.com>
2014-04-15 02:05:27 +0530

    Upload file improved.

--- upload_file.php
+++ upload_file.php
@@ -26,20 +26,25 @@
                 echo "Failed to connect to MySQL: " . mysqli_connect_error();
                 }
 
-            $result = mysqli_query($con,"SELECT file_id, file_hash FROM filesystem");
+            $result = mysqli_query($con,"SELECT file_id, file_hash, owner FROM filesystem ORDER BY file_id");
 
             if($result)
             {
                 while($row = mysqli_fetch_array($result))
                     {
+                        //echo $row['owner'];
                         if($row['file_hash']==$file_hash)
                         {
+                            //echo $row['file_hash']."  ".$row['file_id']."   ".$row['owner'];
                             if($row['owner']==$owner)
                             {
+                                //echo $row['owner'];
                                 $already_present = 2;
+                                
                             }
                             else 
                             {
+                                //echo $row['owner'];
                                 $already_present=1;
                                 $file_id = $row['file_id']; 
                             }
@@ -47,11 +52,12 @@
                         };
                         $file_id = $row['file_id'];
                     }
-            
+                //echo $already_present;
                 if($already_present==0)
                 {
                     $file_id += 1;
                 }
+
                 $temp = explode(".", $_FILES["file"]["name"]);
                 $ext = end($temp);
                 $file_name = $_FILES["file"]["name"] ;
@@ -74,14 +80,31 @@
                 elseif($already_present==1)
                 {
                     $directory_to_upload = $_POST['directory_path'];
+                    $result_new = mysqli_query($con,"SELECT * FROM filesystem WHERE file_hash='$file_hash' ORDER BY owner");
+                    if($result_new)
+                    {
+                      
+                        echo $row1['file_id']."   ".$row1['owner']."   ".$row1['directory_path'];
+                        while($row1=mysqli_fetch_array($result_new))
+                        {
+                            if($row1['owner']==$owner && $row1['directory_path']==$directory_to_upload)
+                            {
+                                echo "File already present.";
+                                die();
+                            }
+                        }
+                    }
+   
                     $query = 'INSERT INTO filesystem (file_id, file_name, owner, file_hash, directory_path, isFolder) VALUES '. "('$file_id', '$file_name', '$owner', '$file_hash', '$directory_to_upload', '0')";
                     $retval = mysqli_query($con, $query);
                     if(! $retval )
                     {
+                        echo "Some error occured with file uploading. Try again.";
                         echo mysqli_connect_error();
                         die();
                     }
                     echo 'Uploaded Successfuly.';
+                    
                 }
                 elseif($already_present==2)
                 {
