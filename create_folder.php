35dee782147ddba8e875d5e88a9f95db58ed794d
ranu010101 <ranu010101@outlook.com>
2014-04-15 17:33:53 +0530

    Minor modifications.

--- create_folder.php
+++ create_folder.php
@@ -1,7 +1,12 @@
 <?php
+    
+    //Start session to access session variables.
     session_start();
+
+    //Check if input is received.
     if(isset($_POST) && !empty($_POST['folder_name']))
     {
+        //Set variables for updating database.
         $owner = $_SESSION['user'];
         $file_id = "";
         $_POST['folder_name'] = str_replace(" ", "_", $_POST['folder_name']);
@@ -9,6 +14,8 @@
         $is_folder = 1;
         $directory_path = $_POST['directory_path'];
         $already_present = 0;
+
+        //Establish connection to MySQL database.
         $con=mysqli_connect("localhost","root","r00tpass","mysql_db");
             // Check connection
         if (mysqli_connect_errno())
@@ -17,7 +24,7 @@
             die();
         }
 
-        $result = mysqli_query($con,"SELECT file_id, file_hash, owner FROM filesystem ORDER BY file_id");
+        $result = mysqli_query($con,"SELECT file_id, file_hash, owner FROM filesystem ORDER BY file_id"); //Query to perform.
 
         /*if($result)
         {
@@ -54,8 +61,11 @@
         {
             echo "Unable to connect to database."
         }*/
+
+        //Traverse through result.
         while($row = mysqli_fetch_array($result))
         {
+            //Check if same user has same file at same directory path.
             if($row['owner']==$owner && $row['directory_path']==$directory_path)
             {
                 $already_present = 1;
@@ -63,16 +73,24 @@
             }    
             $file_id = $row['file_id'];
         }
+
+        //If user has same folder at same directory path.
         if($already_present==1)
         {
             echo "Folder already present.";
             die();
         }
+        
+        //Create Folder
         else
         {
-            $file_id += 1;
+            $file_id += 1; //Increment file_id.
+
+            //Perform Query.
             $insert_query = 'INSERT INTO filesystem (file_id, file_name, owner, directory_path, isFolder) VALUES '. "('$file_id', '$file_name', '$owner', '$directory_path', '1')";
             $insert_result = mysqli_query($con, $insert_query);
+
+            //If query is performed successfully.
             if($insert_result)
             {
                 echo "Folder created successfully.";
@@ -81,6 +99,7 @@
             {
                 echo "Unable to insert.";    
             }
+
         }
         mysqli_close($con);
     }
--- populate.php
+++ populate.php
@@ -1,21 +1,33 @@
 <?php
     
+    //Start session to access session variables.
     session_start();
+
     echo '<link rel="stylesheet" href="css/dropdown.css" type="text/css">';
+    
+    //Set variables to perform database query.
     $user = $_SESSION['user'];
     $folder = $_POST['folder'];
+
+    //Establish connection.
     $query = "SELECT * FROM filesystem WHERE owner='$user' AND directory_path = '$folder' ORDER BY isFolder DESC";
     $con=mysqli_connect("localhost","root","r00tpass","mysql_db");
     if (mysqli_connect_errno())
     {
+        //Unable to establish connection.
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
 
+    //Perform query.
     $result = mysqli_query($con, $query);
+    
+    //If query is performed unsuccessfully.
     if(!$result)
     {
         echo "Unable to access database.<br>";
     }
+
+    //Create options table.
     echo '<div style="margin-top:-10px">';
     echo '<div class="tablehead">';
     echo '
@@ -29,6 +41,8 @@
             </tr>
         </table> 
     ';
+
+    //File parameter table.
     echo '
         <table width="79%" cellpadding="8px">
             <tr class="border_bottom">
@@ -41,13 +55,17 @@
     echo '</div>';
     echo '<div class="tables">';
     echo '<table width="80%" cellpadding="10px" class="table">';
+
+    //Traverse through query results.
     while($row = mysqli_fetch_array($result))
     {
         $name = $row['file_name'];
+
+        //Perform these operations for files only.
         if(!(substr($name, 0, 6) == "dwalin")) 
         {
             $temp = explode(".", $row['file_name']);
-            $ext = end($temp);
+            $ext = end($temp);  //Extension of file obtained.
             echo '<tr class="border_bottom" onclick="showopt()"><td width="5%" class="data">';
             if($ext=="png"||$ext=="jpg"||$ext=="jpeg"||$ext=="gif")
                 echo "<img src='images/picture1.png'>";
@@ -60,18 +78,18 @@
             else
                  echo "<img src='images/exec1.png'>";
             echo"</td>";
-            /*if($ext=="png"||$ext=="jpg"||$ext=="jpeg"||$ext=="gif")
-                echo '<td width="50%" onclick="displayIFrame(\'files/'.$row['file_id'].'.'.$ext.'\')">';
-            else{
-                echo '<td width="50%" onclick="displayOther(\'files/'.$row['file_id'].'.'.$ext.'\')">';
-            }*/
+            
+            //Provide preview.
             if($ext=="png"||$ext=="jpg"||$ext=="jpeg"||$ext=="gif")
                 echo '<td width="50%"><a class="contents" onclick="displayIFrame(\'files/'.$row['file_id'].'.'.$ext.'\');">'.$row['file_name']."</a>";
             else{
                 echo '<td width="50%"><a class="contents" onclick="displayOther(\'files/'.$row['file_id'].'.'.$ext.'\');">'.$row['file_name']."</a>";
             }
+
             echo "</td>";
             echo'<td width="20%" class="data">';
+            
+            //Show file types.
             if($ext=="png"||$ext=="jpg"||$ext=="jpeg"||$ext=="gif")
                 echo 'Image';
             elseif($ext=="doc"||$ext=="pdf"||$ext=="PDF"||$ext=="ppt"||$ext=="pps"||$ext=="pptx"||$ext=="sdf"||$ext=="dat"||$ext=="docx"||$ext=="log"||$ext=="msg"||$ext=="odt"||$ext=="pages"||$ext=="rtf"||$ext=="tex"||$ext=="txt"||$ext=="wpd"||$ext=="wps")
@@ -88,11 +106,13 @@
             echo"</td>";
             echo "</tr>";
         }
+
+        //For folders.
         else{
            $temp = explode(".", $row['file_name']);
             $ext = end($temp);
-            $folder_name = substr($name, 6) ;
-            $folder_name = str_replace("_", " ", $folder_name);
+            $folder_name = substr($name, 6) ; //Remove dwalin tag.
+            $folder_name = str_replace("_", " ", $folder_name); //Replace underscore by blank spaces.
             echo '<tr class="border_bottom"><td width="5%" class="data"><img src="images/folder.png" /></td><td class="data" width="50%" onclick="getFiles(\''.$folder.substr($name, 6).'!\')">'; 
             echo $folder_name."<br>";
             echo "</td>";
